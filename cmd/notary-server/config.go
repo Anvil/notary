1e7bddd48664cc3865826136b598ce3286c7f255
Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>
2016-03-23 17:18:56 -0700

    Use consistent meta storage for server binary
    Signed-off-by: Riyaz Faizullabhoy <riyaz.faizullabhoy@docker.com>

--- cmd/notary-server/config.go
+++ cmd/notary-server/config.go
@@ -79,7 +79,7 @@ func getStore(configuration *viper.Viper, hRegister healthRegister) (
 			return nil, err
 		}
 		s, err := storage.NewSQLStorage(storeConfig.Backend, storeConfig.Source)
-		store = s
+		store = storage.ConsistentMetaStorage{s}
 		hRegister("DB operational", s.CheckHealth, time.Second*60)
 	case notary.RethinkDBBackend:
 		var sess *gorethink.Session
@@ -90,7 +90,7 @@ func getStore(configuration *viper.Viper, hRegister healthRegister) (
 		sess, err = rethinkdb.Connection(storeConfig.CA, storeConfig.Source, storeConfig.AuthKey)
 		if err == nil {
 			s := storage.NewRethinkDBStorage(storeConfig.DBName, sess)
-			store = s
+			store = storage.ConsistentMetaStorage{s}
 			hRegister("DB operational", s.CheckHealth, time.Second*60)
 		}
 	default:
